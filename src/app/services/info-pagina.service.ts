17f3e9fd3016edea1cf13e03f3f7a658975c1b6f
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-11-09 22:55:10 -0500

    Se creo interface con los datos del JSON de info-data.json y se uso en el servicio info-pagina.service.ts

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Infopagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: any = {};
+  info: Infopagina = {};
   cargada = false;
 
   constructor(private http: HttpClient) {
 
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe(response => {
+      .subscribe((response: Infopagina) => {
         this.cargada = true;
         this.info = response;
 
