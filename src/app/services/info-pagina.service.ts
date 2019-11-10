9afde282db51f21fbbf69cf1feda59844a14a418
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-11-09 22:24:42 -0500

    Se esta consumiendo el JSON de info-pagina-service.json

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  constructor() {
-    console.log("Servicio de página cargada");
+  constructor(private http: HttpClient) {
+
+    // console.log("Servicio de página cargada");
+
+    //Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe(response => {
+        console.log(response['twitter']);
+      });
+
   }
 }
